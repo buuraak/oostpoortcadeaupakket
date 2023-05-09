b5c6bf8c3d05772199e43b6e203cba5373fb51f7
Burak <burak@brandclick.nl>
2023-05-09 12:50:21 +0200

    upload database configuration folder

---
+++ database/migrations/2022_06_28_180615_create_participants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->string('p_name');
+            $table->string('p_email');
+            $table->boolean('verified')->default(0);
+            $table->boolean('reminder_sent')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+};
